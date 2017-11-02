fd8d16e01f82c2355849d49b3c52db4ad57bb07e
Gil Barbara <gilbarbara@gmail.com>
2017-11-02 21:37:06 -0200

    refactor user saga

--- app/scripts/sagas/user.js
+++ app/scripts/sagas/user.js
@@ -6,10 +6,9 @@
  */
 
 import { delay } from 'redux-saga';
-import { all, call, fork, put, take } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
 
 import { ActionTypes } from 'constants/index';
-import { goTo } from 'actions';
 
 /**
  * Login
@@ -21,7 +20,6 @@ function* login() {
     yield put({
       type: ActionTypes.USER_LOGIN_SUCCESS,
     });
-    yield put(goTo('/private'));
   } catch (err) {
     /* istanbul ignore next */
     yield put({
@@ -41,7 +39,6 @@ function* logout() {
     yield put({
       type: ActionTypes.USER_LOGOUT_SUCCESS,
     });
-    yield put(goTo('/'));
   } catch (err) {
     /* istanbul ignore next */
     yield put({
@@ -50,23 +47,12 @@ function* logout() {
     });
   }
 }
-
-export function* watchLogin() {
-  yield take(ActionTypes.USER_LOGIN_REQUEST);
-  yield call(login);
-}
-
-export function* watchLogout() {
-  yield take(ActionTypes.USER_LOGOUT_REQUEST);
-  yield call(logout);
-}
-
 /**
  * User Sagas
  */
 export default function* root() {
   yield all([
-    fork(watchLogin),
-    fork(watchLogout),
+    takeLatest(ActionTypes.USER_LOGIN_REQUEST, login),
+    takeLatest(ActionTypes.USER_LOGOUT_REQUEST, logout),
   ]);
 }
--- test/sagas/user.spec.js
+++ test/sagas/user.spec.js
@@ -1,43 +1,43 @@
-import user, { watchLogin, watchLogout } from 'sagas/user';
+import user from 'sagas/user';
 import { ActionTypes } from 'constants/index';
 
-describe('user', () => {
-  it('should have the expected watchers', () => {
-    const saga = user();
-    const watchers = saga.next().value.ALL;
+function getSaga(sagas, action) {
+  const saga = sagas
+    .filter(d => d.FORK.args[0] === action)
+    .map(d => d.FORK.args[1])
+    .reduce((acc, d) => d);
 
+  return saga();
+}
+
+describe('user', () => {
+  let rootSaga;
+  let watchers;
+
+  beforeAll(() => {
+    rootSaga = user();
+    watchers = rootSaga.next().value.ALL;
+  });
+
+  it('should have the expected watchers', () => {
     expect(watchers).toHaveLength(2);
     expect(watchers).toMatchSnapshot();
-    expect(saga.next().done).toBe(true);
+    expect(rootSaga.next().done).toBe(true);
   });
 
   it('should match the login saga', () => {
-    const watcher = watchLogin();
-    expect(watcher.next().value).toMatchSnapshot();
+    const login = getSaga(watchers, ActionTypes.USER_LOGIN_REQUEST);
 
-    const loginCall = watcher.next().value;
-    expect(watcher.next().done).toBe(true);
-    expect(typeof loginCall.CALL.fn).toBe('function');
-
-    const login = loginCall.CALL.fn();
     expect(login.next().value.CALL.args).toEqual([400]);
     expect(login.next().value.PUT.action.type).toBe(ActionTypes.USER_LOGIN_SUCCESS);
-    expect(login.next().value.PUT.action.type).toEqual('@@router/CALL_HISTORY_METHOD');
     expect(login.next().done).toBe(true);
   });
 
   it('should match the logout saga', () => {
-    const watcher = watchLogout();
-    expect(watcher.next().value).toMatchSnapshot();
+    const logout = getSaga(watchers, ActionTypes.USER_LOGOUT_REQUEST);
 
-    const logoutCall = watcher.next().value;
-    expect(watcher.next().done).toBe(true);
-    expect(typeof logoutCall.CALL.fn).toBe('function');
-
-    const logout = logoutCall.CALL.fn();
     expect(logout.next().value.CALL.args).toEqual([200]);
     expect(logout.next().value.PUT.action.type).toBe(ActionTypes.USER_LOGOUT_SUCCESS);
-    expect(logout.next().value.PUT.action.type).toEqual('@@router/CALL_HISTORY_METHOD');
     expect(logout.next().done).toBe(true);
   });
 });
